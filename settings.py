cd28681facf0ad887192f847adfa4b1219abb7f7
OyugoObonyo <nyikal1999@gmail.com>
2021-08-12 16:24:39 +0300

    Store all window settings in a class

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+TODO: Document module
+"""
+
+
+class Settings:
+    """TODO: Document Class"""
+
+    def __init__(self):
+        """TODO:Document method"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (230, 230, 230)
