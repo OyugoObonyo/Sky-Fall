0bf87644c33179707c2fbf44550ad9b4d6fa98b9
OyugoObonyo <nyikal1999@gmail.com>
2021-08-12 16:11:20 +0300

    Fix styling issues

--- window.py
+++ window.py
@@ -1,8 +1,9 @@
 """
-TODO: ADD MODULE DOCUMENTATION! 
+TODO: ADD MODULE DOCUMENTATION!
 """
 import sys
 import pygame
+
 
 class SpaceInvasion:
     """Overall class to manage game assets and behavior."""
@@ -12,7 +13,7 @@ class SpaceInvasion:
         # Assign surface i.e where game elements can be displayed
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Oyugo's Space Invasion")
-
+        self.bg_colour = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -22,10 +23,11 @@ class SpaceInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Make the most recently drawn screen visible.
+            self.screen.fill(self.bg_colour)
             pygame.display.flip()
 
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
-    ai = AlienInvasion()
-    ai.run_game()
+    si = SpaceInvasion()
+    si.run_game()
