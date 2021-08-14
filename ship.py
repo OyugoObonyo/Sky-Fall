1966c4ea01aaaf60a5280b5c72428074c5f485f4
OyugoObonyo <nyikal1999@gmail.com>
2021-08-14 14:39:57 +0300

    Add ship module

---
+++ ship.py
@@ -0,0 +1,20 @@
+"""
+TODO:Add module documentation
+"""
+import pygame
+
+
+class Ship:
+    """TODO: Class documentation"""
+
+    def __init__(self, space_invasion):
+        """ TODO: Document method"""
+        self.screen = space_invasion.screen
+        self.screen_rect = space_invasion.screen.get_rect()
+        self.image = pygame.image.load("IMAGES/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """TODO: Document method"""
+        self.screen.blit(self.image, self.rect)
