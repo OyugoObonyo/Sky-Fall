b6bc7adbdf56eda9d98895c3f5fae30d26422ac2
OyugoObonyo <nyikal1999@gmail.com>
2021-08-15 14:11:29 +0300

    ADD: Update() method and instance attributes

--- ship.py
+++ ship.py
@@ -14,6 +14,12 @@ class Ship:
         self.image = pygame.image.load("E:\Space-Invasion\IMAGES\ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+
+    def update_movement(self):
+        """TODO: Add documentation"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """TODO: Document method"""
