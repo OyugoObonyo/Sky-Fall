9b4782a576bbc626267e0a68999a5bc696d75ad5
OyugoObonyo <nyikal1999@gmail.com>
2021-08-16 16:01:06 +0300

    Add bullet attributes

--- settings.py
+++ settings.py
@@ -3,6 +3,9 @@ TODO: Document module
 """
 
 
+from _typeshed import Self
+
+
 class Settings:
     """TODO: Document Class"""
 
@@ -12,3 +15,8 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_colour = (230, 230, 230)
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
