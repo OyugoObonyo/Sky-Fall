6c57f46cc09bd348b26cf572b27074079a163825
OyugoObonyo <nyikal1999@gmail.com>
2021-08-16 16:21:54 +0300

    Update Bullet class' methods

--- bullet.py
+++ bullet.py
@@ -18,3 +18,14 @@ class Bullet(Sprite):
         self.rect.midtop = space_invasion.ship.rect.midtop
         # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Moves the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+ 
+    def draw_bullet(self):
+        """Draws the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
