ee9977b335e368729713ac871b6a512b4b9a5ee0
OyugoObonyo <nyikal1999@gmail.com>
2021-08-17 15:36:44 +0300

    FIX: Linter warnings

--- scoreboard.py
+++ scoreboard.py
@@ -5,16 +5,17 @@ import pygame.font
 from pygame.sprite import Group
 from ship import Ship
 
+
 class Scoreboard:
- """A class to report scoring information."""
- 
-    def __init__(self, space_invasion):
+    """A class to report scoring information."""
+
+    def __init__(self, sky_fall):
         """Initialize scorekeeping attributes."""
-        self.space_invasion = space_invasion
-        self.screen = space_invasion.screen
+        self.sky_fall = sky_fall
+        self.screen = sky_fall.screen
         self.screen_rect = self.screen.get_rect()
-        self.settings = space_invasion.settings
-        self.stats = space_invasion.stats
+        self.settings = sky_fall.settings
+        self.stats = sky_fall.stats
         # Font settings for scoring information.
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
@@ -28,7 +29,7 @@ class Scoreboard:
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True,self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render(score_str, True,self.text_color, self.settings.bg_colour)
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
@@ -37,7 +38,7 @@ class Scoreboard:
     def prep_level(self):
         """Turn the level into a rendered image."""
         level_str = str(self.stats.level)
-        self.level_image = self.font.render(level_str, True,self.text_color, self.settings.bg_color)
+        self.level_image = self.font.render(level_str, True,self.text_color, self.settings.bg_colour)
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
@@ -54,7 +55,7 @@ class Scoreboard:
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True,self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True,self.text_color, self.settings.bg_colour)
         # Center the high score at the top of the screen.
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
@@ -70,7 +71,7 @@ class Scoreboard:
         """Show how many ships are left."""
         self.ships = Group()
         for ship_number in range(self.stats.ships_left):
-            ship = Ship(self.space_invasion)
+            ship = Ship(self.sky_fall)
             ship.rect.x = 10 + ship_number * ship.rect.width
             ship.rect.y = 10
             self.ships.add(ship)
--- settings.py
+++ settings.py
@@ -9,20 +9,21 @@ class Settings:
         """TODO:Document method"""
         self.ship_speed = 1.5
         self.ship_limit = 3
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_colour = (230, 230, 230)
+        self.bg_colour = (0, 0, 0)
         # Bullet settings
         self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
-        # Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
+        # Star settings
+        self.star_speed = 1.0
+        self.galaxy_drop_speed = 10
+        # galaxy_direction of 1 represents right; -1 represents left.
+        self.galaxy_direction = 1
         # Game speed
         self.speedup_scale = 1.1
         self.score_scale = 1.5
@@ -32,15 +33,15 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
-        self.alien_points = 50
+        self.star_speed = 1.0
+        # galaxy_direction of 1 represents right; -1 represents left.
+        self.galaxy_direction = 1
+        self.star_points = 50
 
     def increase_speed(self):
         """Increase speed settings."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
-        self.alien_speed *= self.speedup_scale
-        self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
+        self.star_speed *= self.speedup_scale
+        self.star_points = int(self.star_points * self.score_scale)
+        print(self.star_points)
